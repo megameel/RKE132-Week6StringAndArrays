9dbf048ace3fcd591fa18e477bd14edc99a091f4
Andres Hiis <andres.hiis@tktk.ee>
2023-10-16 21:53:04 +0300

    add week6 code

---
+++ eek6StringsAndArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿string hello = " we are the champions?";
+
+int stringLength = hello.Length;
+hello = hello.Trim();
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
+int wordCounter = 1;
+for(int i = 0; i < hello.Length; i++)
+{
+    if (char.IsWhiteSpace(hello[i]))
+    {
+        wordCounter++; 
+    }
+}
+Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
